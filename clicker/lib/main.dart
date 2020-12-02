e5aa92963a4706bd4f89d5d1db147c61391da075
Maxime Britto <maxime@purplegiraffe.fr>
2020-12-02 17:47:29 +0100

    13_5_Rendre_lutilisateur_fou

--- clicker/lib/main.dart
+++ clicker/lib/main.dart
@@ -52,8 +52,23 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             if (_counter > 0) Text("Continuez à cliquer"),
             if (_counter > 5 && _counter < 10) Text("OK...pas mal"),
-            if (_counter >= 10)
-              Text("Les choses sérieuses peuvent commencer :)")
+            if (_counter >= 10 && _counter < 20)
+              Text("Les choses sérieuses peuvent commencer :)"),
+            if (_counter >= 20 && _counter < 35)
+              Text("J'aime les panoramas. (OSS 117)"),
+            if (_counter >= 35 && _counter < 60)
+              Text("J'aime me battre. (OSS 117)"),
+            if (_counter >= 60 && _counter < 120)
+              Text("J'aime me beurrer la biscotte. (OSS 117)"),
+            if (_counter >= 120 && _counter < 200)
+              Text("J'aime le bruit blanc de l'eau. (OSS 117)"),
+            if (_counter >= 200 && _counter < 201)
+              Text("J'aime quand on m'enduit d'huile. (OSS 117)"),
+            if (_counter >= 201 && _counter < 350)
+              Text(
+                  "J'aime quand une jolie femme brune m'apporte mon petit déjeuner au lit. (OSS 117)"),
+            if (_counter >= 350)
+              Text("Vous dites ça à toutes les femmes. Larmina")
           ],
         ),
       ),
